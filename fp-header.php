e38140ed5d34f67189d42b33341ff6ab2c742ff5
e2tha-e <ee@electric-eloquence.io>
2017-06-23 09:07:02 -0400

    style updates, minor template refactor

--- fp-header.php
+++ fp-header.php
@@ -2,10 +2,11 @@
 <header class="header cf" role="banner">
 	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="logo">
 		<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/_assets/src/logo.svg" alt="<?php echo get_bloginfo(); ?>" />
+
 	</a>
 <a href="#" class="nav-toggle nav-toggle-search icon-search"></a>
-		<a href="#" class="nav-toggle nav-toggle-menu icon-menu"></a>
-		<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
+	<a href="#" class="nav-toggle nav-toggle-menu icon-menu"></a>
+	<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
 		<div id="widget-area" class="widget-area" role="complementary">
 			<?php dynamic_sidebar( 'sidebar' ); ?>
 		</div><!-- .widget-area -->
